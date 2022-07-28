2f4d2e2230e1f466ae4a403ed8513868d5979070
hermitex <mercytibs@yahoo.com>
2022-07-28 20:52:32 +0300

    modify app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import { Container } from "reactstrap";
 import { AuthProvider } from "./components/context/AuthContext";
 import Signup from "./components/signup/Signup";
-import logo from "./logo.svg";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/login/Login";
+import Home from "./components/home/Home";
+import Navigation from "./components/navigation/Navigation";
+import Listings from "./components/listings/Listings";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <Container
-          className="d-flex align-items-center just-content-center"
-          style={{ height: 100 + "vh" }}
-        >
-          <Routes>
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </Container>
+        <Navigation />        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />.
+          <Route path="/listings" element={<Listings />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
       </AuthProvider>
     </Router>
   );
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Card, CardImg, CardImgOverlay, CardText, CardTitle } from "reactstrap";
+import home1 from "./images/home-1.jpg";
+import './Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+  <div><h1>Home</h1></div>
+  );
 }
 
-export default Home
+export default Home;
