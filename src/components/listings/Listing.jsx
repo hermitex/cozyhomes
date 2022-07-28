39b7da50e93b368bbede949f7a141e1e45f53744
hermitex <mercytibs@yahoo.com>
2022-07-28 20:51:37 +0300

    modify listings

---
+++ src/components/listings/Listing.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import {
+  Col,
+  Card,
+  CardSubtitle,
+  CardTitle,
+  CardBody,
+  Button,
+  CardText,
+  ListGroup,
+  ListGroupItem,
+} from "reactstrap";
+
+function Listing({ listing }) {
+  return (
+    <Col style={{ backgroundColor: "#e5e5e5" }}>
+      <Card
+        color="secondary"
+        className="mb-3"
+        inverse
+        style={{
+          width: "100%",
+        }}
+      >
+        <img
+          id="listing-image"
+          alt="Card image"
+          src="https://picsum.photos/300/200"
+        />
+        <i className="fa-solid fa-heart" id="wish-icon"></i>
+        <CardBody>
+          <CardTitle tag="h5">
+            <ul className="list-unstyled d-flex justify-content-between align-content-center">
+              <li>{listing.name}</li>
+              <li>
+                <i className="fa-solid fa-share-nodes"></i>
+              </li>
+            </ul>
+          </CardTitle>
+
+          <CardSubtitle className="mb-2 text-muted" tag="h6">
+            {listing.location}, {listing.county} county
+          </CardSubtitle>
+
+          <CardSubtitle className="mb-2 text-light" tag="h6">
+            {listing.price}
+          </CardSubtitle>
+          <ListGroup
+            horizontal
+            className="d-flex text-center"
+            style={{
+              width: "100%",
+              marginTop: "2rem",
+            }}
+          >
+            <ListGroupItem
+              tag="a"
+              style={{
+                width: "100%",
+              }}
+            >
+              <i className="fa-solid fa-bed"></i>
+              {listing.bedrooms}
+            </ListGroupItem>
+            <ListGroupItem
+              tag="a"
+              style={{
+                width: "100%",
+              }}
+            >
+              <i className="fa-solid fa-shower"></i>
+              {listing.bathrooms}
+            </ListGroupItem>
+            <ListGroupItem
+              tag="a"
+              style={{
+                width: "100%",
+              }}
+            >
+              <var>{listing.sqrt}</var>
+              <sup>2</sup> ft
+            </ListGroupItem>
+          </ListGroup>
+        </CardBody>
+      </Card>
+    </Col>
+  );
+}
+
+export default Listing;
---
+++ src/components/listings/Listings.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Row } from "reactstrap";
+import useFetch from "../hooks/useFetch";
+import Search from "../search/Search";
+import Listing from "./Listing";
+
+function Listings() {
+  const [listings] = useFetch("http://localhost:8002/properties");
+  return (
+    <>
+      <Search />
+      <Container
+        className="d-flex  just-content-center mt-5"
+        style={{ height: 100 + "vh" }}
+      >
+        <Row>
+          {listings &&
+            listings.map((listing) => (
+              <Listing key={listing.id} listing={listing} />
+            ))}
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default Listings;
